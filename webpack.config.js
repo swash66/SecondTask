41b09bb185e1eeb83be9b34d2908b117f63d7566
Roman Nemnov <swash5@rambler.ru>
2018-08-28 03:01:07 +0700

    28.08

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ let path = require('path');
 let ExtractTextPlugin = require("extract-text-webpack-plugin");
 let jeet = require('jeet');
 let nib = require('nib');
+let sync = require('browser-sync-webpack-plugin');
 
 let conf = {
     entry: './src/index.js',
@@ -49,8 +50,36 @@ let conf = {
             }
         ]
     },
+    watch: true,
     plugins: [
+        new sync({
+            host: "localhost",
+            port: 6555,
+            server: {
+                baseDir: "./"  // здесь д.б. index.html, не регариует на ../
+            },
+            open: false,
+            files: ["./*.html"]
+        }),
         new ExtractTextPlugin("styles.css"),
+        /*new BrowserSyncPlugin(
+            // BrowserSync options
+            {
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 7331,
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:3100/)
+                // through BrowserSync
+                proxy: 'http://localhost:8080/'
+            },
+            // plugin options
+            {
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
+            }
+        )*/
         /*new HtmlWebpackPlugin({
             template: './src/index.pug',
         })*/
